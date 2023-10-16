f325cc78316cb01e37762943ae47a497ef3db93e
Joel Storr <joel.storr@ewe.net>
2023-10-16 16:57:58 +0200

    Initial Commit

---
+++ RepoWatcher/RepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherApp.swift
+//  RepoWatcher
+//
+//  Created by Joel Storr on 16.10.23.
+//
+
+import SwiftUI
+
+@main
+struct RepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
