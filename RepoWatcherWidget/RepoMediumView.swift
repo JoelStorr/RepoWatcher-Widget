0a32cb5898682f67c8d80879a70a710049fa1714
Joel Storr <joel.storr@ewe.net>
2023-10-18 21:32:32 +0200

    Reworkd Repository structure to handle Bigger repo widget ui

---
+++ RepoWatcherWidget/RepoMediumView.swift
@@ -0,0 +1,92 @@
+//
+//  RepoMediumView.swift
+//  RepoWatcher
+//
+//  Created by Joel Storr on 18.10.23.
+//
+
+import SwiftUI
+import WidgetKit
+
+struct RepoMediumView: View {
+    let repo: Repository
+    let formatter = ISO8601DateFormatter()
+    var daysSinceLastActivity: Int {
+        calculateDaysSinceLastActivity(from: repo.pushedAt)
+    }
+
+    var body: some View {
+        HStack{
+            VStack(alignment:.leading){
+                HStack{
+                    Image(uiImage: UIImage(data: repo.avatarData) ?? UIImage(resource: .avatar))
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .clipShape(Circle())
+                    Text(repo.name)
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .minimumScaleFactor(0.6)
+                        .lineLimit(1)
+                }
+                .padding(.bottom, 6)
+                HStack{
+                    StateLabel(value: repo.watchers, systemImageName: "star.fill")
+                    StateLabel(value: repo.forks, systemImageName: "tuningfork")
+                    if repo.hasIssues{
+                        StateLabel(value: repo.openIssues, systemImageName: "exclamationmark.triangle.fill")
+                    }
+                }
+            }
+            Spacer()
+            VStack{
+                Text("\(daysSinceLastActivity)")
+                    .fontWeight(.bold)
+                    .font(.system(size: 70))
+                    .frame(width: 90)
+                    .minimumScaleFactor(0.6)
+                    .lineLimit(1)
+                    .foregroundStyle(daysSinceLastActivity > 50 ? Color.pink : Color.green)
+                
+                Text("days ago")
+                    .font(.caption2)
+                    .foregroundStyle(Color.secondary)
+            }
+        }
+        .padding()
+    }
+    
+    
+    func calculateDaysSinceLastActivity(from dateString: String) -> Int {
+        let lastActivityData = formatter.date(from: dateString) ?? .now
+        let daysSinceLastActivity = Calendar.current.dateComponents([.day], from: lastActivityData, to: .now).day ?? 0
+        return daysSinceLastActivity
+    }
+}
+
+struct RepoMediumView_Previews: PreviewProvider {
+    static var previews: some View{
+        RepoMediumView(repo: Repository.placeholder)
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
+            
+    }
+}
+
+
+
+fileprivate struct StateLabel: View {
+    
+    let value : Int
+    let systemImageName: String
+    
+    var body: some View {
+        Label{
+            Text("\(value)")
+                .font(.footnote)
+        } icon: {
+            Image(systemName: systemImageName)
+                .foregroundStyle(Color.green)
+        }
+        .fontWeight(.medium)
+    }
+}
--- RepoWatcherWidget/RepoWatcherWidget.swift
+++ RepoWatcherWidget/RepoWatcherWidget.swift
@@ -10,11 +10,11 @@ import SwiftUI
 
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> RepoEntry {
-        RepoEntry(date: Date(), repo: Repository.placeholder, avatarImageData: Data())
+        RepoEntry(date: Date(), repo: Repository.placeholder, bottomRepo: Repository.placeholder)
     }
 
     func getSnapshot(in context: Context, completion: @escaping (RepoEntry) -> ()) {
-        let entry = RepoEntry(date: Date(), repo: Repository.placeholder, avatarImageData: Data())
+        let entry = RepoEntry(date: Date(), repo: Repository.placeholder, bottomRepo: nil)
         completion(entry)
     }
 
@@ -24,9 +24,10 @@ struct Provider: TimelineProvider {
             let nextUpdate = Date().addingTimeInterval(43200) //12 hours in seconds
             
             do{
-                let repo = try await NetworkManager.shared.getRepo(atUrl: RepoURL.swiftNews)
+                var repo = try await NetworkManager.shared.getRepo(atUrl: RepoURL.swiftNews)
                 let avatarImageData = await NetworkManager.shared.downloadImageData(from: repo.owner.avatarUrl)
-                let entry = RepoEntry(date: .now, repo: repo, avatarImageData: avatarImageData ?? Data())
+                repo.avatarData = avatarImageData ?? Data()
+                let entry = RepoEntry(date: .now, repo: repo)
                 let timeline = Timeline(entries: [entry], policy: .after(nextUpdate))
                 completion(timeline)
             }catch{
@@ -39,64 +40,33 @@ struct Provider: TimelineProvider {
 struct RepoEntry: TimelineEntry {
     let date: Date
     let repo: Repository
-    let avatarImageData: Data
+    let bottomRepo: Repository?
 }
 
 struct RepoWatcherWidgetEntryView : View {
+    @Environment(\.widgetFamily) var family
     var entry: RepoEntry
-    let formatter = ISO8601DateFormatter()
-    var daysSinceLastActivity: Int {
-        calculateDaysSinceLastActivity(from: entry.repo.pushedAt)
-    }
+   
+ 
 
     var body: some View {
-        HStack{
-            VStack(alignment:.leading){
-                HStack{
-                    Image(uiImage: UIImage(data: entry.avatarImageData) ?? UIImage(resource: .avatar))
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                        .clipShape(Circle())
-                    Text(entry.repo.name)
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                        .minimumScaleFactor(0.6)
-                        .lineLimit(1)
-                }
-                .padding(.bottom, 6)
-                HStack{
-                    StateLabel(value: entry.repo.watchers, systemImageName: "star.fill")
-                    StateLabel(value: entry.repo.forks, systemImageName: "tuningfork")
-                    if entry.repo.hasIssues{
-                        StateLabel(value: entry.repo.openIssues, systemImageName: "exclamationmark.triangle.fill")
-                    }
-                }
+            
+        switch family{
+        case.systemMedium:
+            RepoMediumView(repo: entry.repo)
+            
+        case .systemLarge:
+            VStack(spacing: 36){
+                RepoMediumView(repo: entry.repo)
+                RepoMediumView(repo: entry.repo)
             }
-            Spacer()
-            VStack{
-                Text("\(daysSinceLastActivity)")
-                    .fontWeight(.bold)
-                    .font(.system(size: 70))
-                    .frame(width: 90)
-                    .minimumScaleFactor(0.6)
-                    .lineLimit(1)
-                    .foregroundStyle(daysSinceLastActivity > 50 ? Color.pink : Color.green)
-                
-                Text("days ago")
-                    .font(.caption2)
-                    .foregroundStyle(Color.secondary)
-            }
+
+        case .systemExtraLarge, .systemSmall, .accessoryCircular, .accessoryRectangular, .accessoryInline:
+            EmptyView()
+        @unknown default:
+            EmptyView()
         }
-        .padding()
     }
-    
-    
-    func calculateDaysSinceLastActivity(from dateString: String) -> Int {
-        let lastActivityData = formatter.date(from: dateString) ?? .now
-        let daysSinceLastActivity = Calendar.current.dateComponents([.day], from: lastActivityData, to: .now).day ?? 0
-        return daysSinceLastActivity
-    }
-    
 }
 
 struct RepoWatcherWidget: Widget {
@@ -115,31 +85,16 @@ struct RepoWatcherWidget: Widget {
         }
         .configurationDisplayName("Repo Watcher")
         .description("This widget show you update stats to you favorite repos.")
-        .supportedFamilies([.systemMedium])
+        .supportedFamilies([.systemMedium, .systemLarge])
     }
 }
 
 #Preview(as: .systemMedium) {
     RepoWatcherWidget()
 } timeline: {
-    RepoEntry(date: .now, repo: Repository.placeholder, avatarImageData: Data())
+    RepoEntry(date: .now, repo: Repository.placeholder, bottomRepo: Repository.placeholder)
    
 }
 
 
-fileprivate struct StateLabel: View {
-    
-    let value : Int
-    let systemImageName: String
-    
-    var body: some View {
-        Label{
-            Text("\(value)")
-                .font(.footnote)
-        } icon: {
-            Image(systemName: systemImageName)
-                .foregroundStyle(Color.green)
-        }
-        .fontWeight(.medium)
-    }
-}
+
