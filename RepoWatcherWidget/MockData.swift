b5f65a39957a24879ad295a271cdcd40a3f23fe8
Joel Storr <joel.storr@ewe.net>
2023-10-20 11:47:31 +0200

    Added new Mock Data

--- RepoWatcherWidget/MockData.swift
+++ RepoWatcherWidget/MockData.swift
@@ -9,6 +9,21 @@ import Foundation
 
 
 struct MockData {
-    static let repoOne = Repository(name: "Repo 1", owner: Owner(avatarUrl: ""), hasIssues: true, forks: 65, watchers: 123, openIssues: 55, pushedAt: "2023-08-09T18:19:30Z", avatarData: Data())
+    static let repoOne = Repository(
+        name: "Repo 1",
+        owner: Owner(avatarUrl: ""),
+        hasIssues: true,
+        forks: 65,
+        watchers: 123,
+        openIssues: 55,
+        pushedAt: "2023-08-09T18:19:30Z",
+        avatarData: Data(),
+        contributors: [
+            Contributor(login: "Sean Allen", avatarUrl: "", contributions: 42, avatarData: Data()),
+            Contributor(login: "Michael Jordan", avatarUrl: "", contributions: 23, avatarData: Data()),
+            Contributor(login: "Steph Curry", avatarUrl: "", contributions: 30, avatarData: Data()),
+            Contributor(login: "LeBron James", avatarUrl: "", contributions: 6, avatarData: Data())
+        ]
+    )
     static let repoTwo = Repository(name: "Repo 2", owner: Owner(avatarUrl: ""), hasIssues: false, forks: 144, watchers: 169, openIssues: 20, pushedAt: "2023-10-09T18:19:30Z", avatarData: Data())
 }
