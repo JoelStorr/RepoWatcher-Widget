16c0638831a3558d667210795babf3cb83cefbc6
Joel Storr <joel.storr@ewe.net>
2023-10-22 21:01:43 +0200

    Added UserDefaults to project for Repo List

--- RepoWatcher/ContentView.swift
+++ RepoWatcher/ContentView.swift
@@ -8,16 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var newRepo = ""
+    @State private var repos: [String] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                HStack {
+                    TextField("Ex. sallen0400/swift-news", text: $newRepo)
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                        .textFieldStyle(.roundedBorder)
+
+                    Button {
+                        if !repos.contains(newRepo) && !newRepo.isEmpty{
+                            repos.append(newRepo)
+                            UserDefaults.shared.setValue(repos, forKey: UserDefaults.repoKey)
+                            newRepo = ""
+                        }else{
+                            print("repo already exist or name is empty")
+                        }
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.green)
+                    }
+                }
+                .padding()
+
+                VStack(alignment: .leading) {
+                    Text("Saved Repos")
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                        .padding(.leading)
+
+                    List(repos, id: \.self) { repo in
+                        Text(repo)
+                            .swipeActions {
+                                Button("Delete") {
+                                    if repos.count > 1 {
+                                        repos.removeAll{ $0 == repo}
+                                        UserDefaults.shared.setValue(repos, forKey: UserDefaults.repoKey)
+                                    }
+                                }
+                                .tint(.red)
+                            }
+                    }
+                }
+            }
+            .navigationTitle("Repo List")
+            .onAppear{
+                guard let retrievedRepos = UserDefaults.shared.value(
+                    forKey: UserDefaults.repoKey
+                ) as? [String] else{
+                    let defaultValues = ["sallen0400/swift-news"]
+                    UserDefaults.shared.set(defaultValues, forKey: UserDefaults.repoKey)
+                    repos = defaultValues
+                    return
+                }
+                
+                repos = retrievedRepos
+            }
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
---
+++ RepoWatcher/UserDefaults+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaults+Ext.swift
+//  RepoWatcher
+//
+//  Created by Joel Storr on 22.10.23.
+//
+
+import Foundation
+
+
+extension UserDefaults{
+    static var shared: UserDefaults{
+        UserDefaults(suiteName: "group.com.joelstorr.RepoWatcher")!
+    }
+    
+    static let repoKey = "repos"
+}
